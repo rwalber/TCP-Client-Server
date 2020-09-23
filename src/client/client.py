e36f5e2452a315c3b99f990f04995e1a850379f9
Walber Rocha <walber_jesus@hotmail.com>
2020-09-22 21:54:10 -0300

    Update
    To receive entries via sys.argv

--- src/client/client.py
+++ src/client/client.py
@@ -1,39 +1,43 @@
 # Author: Walber C J Rocha
 # University: Universidade Federal do Recôncavo da Bahia
 
-import socket, pickle, os
+import os, pickle, socket, sys
 
 BUFFER_SIZE = 1024
 
-if __name__ == "__main__":
-    # host = input('Host: ')
-    # port = input('Port: ')
-
-    host = 'localhost'
-    port = 3333
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, int(port)))
-
-    request = input('Request file: ')
+def client_request(directory, s, request):
+    os.chdir(directory)
     s.send(request.encode())
 
-    if(request == 'cache'):
+    if(request == 'list-cache'):
         files_in_cache = s.recv(BUFFER_SIZE)
         print(pickle.loads(files_in_cache))
         
     else:
-        with open(request, 'wb') as file:
-            while True:
-                data = s.recv(BUFFER_SIZE)
-                if not data:
-                    break
-                # if(pickle.loads(data) == 'The file does not exist on the server'):
-                #     print('The file does not exist on the server')
-                #     os.remove(request)
-                #     break
-                file.write(data)
-        file.close()
-    
+        while True:
+            data = s.recv(BUFFER_SIZE)
+            if(data == b'File does not exist'):
+                print(f'File {request} does not exist in the server')
+                break
+            else:
+                with open(request, 'wb') as file:
+                    if not data:
+                        break
+                    file.write(data)
+                file.close()
+                print(f'File {request} saved')
+                break
     s.close()
-    print('Transfer Completed')
+
+if __name__ == "__main__":
+    
+    HOST = sys.argv[1]
+    PORT = sys.argv[2]
+    DIRECTORY = sys.argv[4]
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, int(PORT)))
+    
+    request = sys.argv[3]
+    
+    client_request(DIRECTORY, s, request)
